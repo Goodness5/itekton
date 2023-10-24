537a092769ba84948849ddeb72bcaefb3d899d70
goodness5 <goodnesskolapo@gmail.com>
2023-10-24 13:39:00 +0100

    static files update

--- itekton/urls.py
+++ itekton/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
@@ -38,3 +40,6 @@ urlpatterns = [
     path('accounts/', include('accounts.urls')),
      path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
