ec1b06640b8677e1f7da5ba91146806dda99e73d
goodness5 <goodnesskolapo@gmail.com>
2023-11-04 00:53:00 +0100

    omo..

--- itekton/settings.py
+++ itekton/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 from decouple import config, Csv
-
+import smtplib
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -187,5 +187,6 @@ EMAIL_PORT = config('EMAIL_PORT', cast=int)
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=True, cast=bool)
-
 DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL')
+server = smtplib.SMTP('smtp.office365.com', 587) 
+server.starttls()
